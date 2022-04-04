757b6c111112eae226cdcbffd56621b229c9c939
Sirawit <popeye-zz@hotmail.com>
2022-04-05 02:40:15 +0700

    finish -> more testing to find bug and error

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BasicCalculator.Models;
+using System.Text;
 
 namespace BasicCalculator.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static CalculatorModel calculator;
+
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,7 +23,64 @@ namespace BasicCalculator.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            calculator = new CalculatorModel();
+            return View(calculator);
+        }
+
+        public IActionResult Operation(string newValue, string operand)
+        {
+            if (operand == "add")
+            {
+                calculator.Value = Convert.ToDouble(newValue);
+                calculator.Operand = "add";
+            }
+            else if (operand == "sub")
+            {
+                calculator.Value = Convert.ToDouble(newValue);
+                calculator.Operand = "sub";
+            }
+            else if (operand == "mul")
+            {
+                calculator.Value = Convert.ToDouble(newValue);
+                calculator.Operand = "mul";
+            }
+            else if (operand == "div")
+            {
+                calculator.Value = Convert.ToDouble(newValue);
+                calculator.Operand = "div";
+            }
+            else if (operand == "equ")
+            {
+                if (calculator.Operand == "add")
+                {
+                    calculator.Value += Convert.ToDouble(newValue);
+                }
+                else if (calculator.Operand == "sub")
+                {
+                    calculator.Value -= Convert.ToDouble(newValue);
+                }
+                else if (calculator.Operand == "mul")
+                {
+                    calculator.Value *= Convert.ToDouble(newValue);
+                }
+                else if (calculator.Operand == "div")
+                {
+                    calculator.Value /= Convert.ToDouble(newValue);
+                }
+                else if (calculator.Operand == "")
+                {
+                    calculator.Value = Convert.ToDouble(newValue);
+                }
+            }
+
+            return Content(Convert.ToString(calculator.Value), "text/plain", Encoding.UTF8);
+        }
+
+        public IActionResult AllClear()
+        {
+            calculator.Value = 0;
+            calculator.Operand = "";
+            return Content(Convert.ToString(calculator.Value), "text/plain", Encoding.UTF8);
         }
 
         public IActionResult About()
---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,8 @@
+namespace BasicCalculator.Models
+{
+    public class CalculatorModel
+    {
+        public double Value { get; set; } = 0.0;
+        public string Operand { get; set; } = "";
+    }
+}
